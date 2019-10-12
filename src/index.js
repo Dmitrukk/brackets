cc6d047c4d484fa7a6a33998f2f7da54f80da085
Dmitrukk <d.dmitrukk@gmail.com>
2019-10-13 00:39:53 +0300

    Commit No.1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let checkSum = 0; // count of open brackets
+    const openBrackets = []; // an array of configuration numbers of open brackets
+  
+    for (let i = 0, n = str.length; i < n; i++) {
+      for (let j = 0, m = bracketsConfig.length; j < m; j++) {
+        // is the configuration of the last open bracket equal the current configuration
+        const condition = openBrackets[openBrackets.length - 1] === j;
+        // a flag of equality of open and closed brackets
+        const areEqual = (bracketsConfig[j][0] === bracketsConfig[j][1]);
+  
+        // is the current bracket an open bracket
+        if (str[i] === bracketsConfig[j][0]) {
+          if (areEqual && condition) {
+            checkSum--;
+            openBrackets.pop();
+            break;
+          } else {
+            checkSum++;
+            openBrackets.push(j);
+            break;
+          }
+        }
+  
+        // is the current bracket a closed bracket
+        if (str[i] === bracketsConfig[j][1]) {
+          if (condition) {
+            checkSum--;
+            openBrackets.pop();
+            break;
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+  
+    return checkSum === 0;  
 }
